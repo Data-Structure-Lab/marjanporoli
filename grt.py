237a7e13a890d3213afbf384873dc426490e0bcf
Marjanporoli <82402143+marjanporoli@users.noreply.github.com>
2022-11-17 11:31:09 +0530

    Delete grt.py

--- grt.py
+++
@@ -1,9 +0,0 @@
-a=5
-b=4
-c=9
-if(a>b) and (a>c):
-    print("a is greater than b and c ")
-    if(b>a) and (b>c):
-        print("b is greater than a and c")
-else:
-    print("c is greater than a and b")
