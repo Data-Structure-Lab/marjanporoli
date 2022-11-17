fe943eab5e7989ed952cfa1384f58a05c643a14c
Marjanporoli <82402143+marjanporoli@users.noreply.github.com>
2022-11-17 11:33:01 +0530

    Delete list.py

--- list.py
+++
@@ -1,9 +0,0 @@
-Python 3.7.5 (default, Dec  9 2021, 17:04:37) 
-[GCC 8.4.0] on linux
-Type "help", "copyright", "credits" or "license()" for more information.
->>> thislist=["tomato","mango","onion","chully"]
->>> print(thislist[-2])
-onion
->>> print(thislist[1])
-mango
->>> 
