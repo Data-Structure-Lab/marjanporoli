e9ca8ef839658a85eeb5f57b0ec06c3d70dccc50
Marjanporoli <82402143+marjanporoli@users.noreply.github.com>
2022-11-17 11:33:42 +0530

    Delete nnumbers.py

--- nnumbers.py
+++
@@ -1,8 +0,0 @@
-sum = 0
-print("Enter the Value of n: ")
-n = int(input())
-print("Enter Numbers: ")
-for i in range(n):
-    num = int(input())
-    sum = sum+num
-  print("Sum of Numbers = " + str(sum))
